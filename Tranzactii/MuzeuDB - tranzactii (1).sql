05f50e92bec1a4b8223911ea408777ec7bce38ab
Cristian Stanciu <crististanciu095@gmail.com>
2023-05-22 18:05:11 +0300

    Refactor code

--- Tranzactii/MuzeuDB - tranzactii (1).sql
+++ Tranzactii/MuzeuDB - tranzactii (1).sql
@@ -5,12 +5,12 @@ CREATE OR ALTER FUNCTION uf_ValidareCuloare (@culoare VARCHAR(50))
 RETURNS BIT
 AS
 BEGIN
-	DECLARE @retval BIT = 1
+	DECLARE @valid BIT = 1
 
     IF (@culoare IS NULL OR @culoare = '')
-        SET @retval = 0
+        SET @valid = 0
 
-	RETURN @retval
+	RETURN @valid
 END
 
 GO
@@ -19,15 +19,15 @@ CREATE OR ALTER FUNCTION uf_ValidareMaterial (@material VARCHAR(50))
 RETURNS BIT
 AS
 BEGIN
-	DECLARE @retval BIT = 1
+	DECLARE @valid BIT = 1
 
     IF (@material IS NULL OR @material = '')
-        SET @retval = 0
+        SET @valid = 0
 	
     IF (@material NOT IN ('Ceramica', 'Argila', 'Lut'))
-        SET @retval = 0;
+        SET @valid = 0;
 
-	RETURN @retval
+	RETURN @valid
 END
 
 GO
@@ -36,15 +36,15 @@ CREATE OR ALTER FUNCTION uf_ValidareVechime (@vechime INT)
 RETURNS BIT
 AS
 BEGIN
-	DECLARE @retval BIT = 1
+	DECLARE @valid BIT = 1
 
     IF (@vechime < 0)
-        SET @retval = 0
+        SET @valid = 0
 	
     IF (@vechime < 100)
-        SET @retval = 0;
+        SET @valid = 0;
 
-	RETURN @retval
+	RETURN @valid
 END
 
 GO
@@ -53,18 +53,18 @@ CREATE OR ALTER FUNCTION uf_ValidareVitrinaID (@vitrinaID INT)
 RETURNS BIT
 AS
 BEGIN
-	DECLARE @retval BIT = 1
+	DECLARE @valid BIT = 1
 
     IF (@vitrinaID < 1)
-        SET @retval = 0
+        SET @valid = 0
 	
     IF (NOT EXISTS (SELECT *
-					FROM [MuzeuDB].[dbo].[Vitrine]
+					FROM Vitrine
 					WHERE VitrinaID = @vitrinaID)
 		)
-        SET @retval = 0;
+        SET @valid = 0;
 
-	RETURN @retval
+	RETURN @valid
 END
 
 GO
@@ -73,22 +73,24 @@ CREATE OR ALTER PROCEDURE sp_ValidareVas
 @culoare VARCHAR(50), @material VARCHAR(50), @vechime INT, @vitrinaID INT
 AS
 BEGIN
-	DECLARE @err VARCHAR(MAX) = ''
+	DECLARE @errors VARCHAR(MAX) = ''
 
 	IF (dbo.uf_ValidareCuloare(@culoare) = 0)
-		SET @err = 'Culoare invalida!' + CHAR(13) + CHAR(10)
+		SET @errors = 'Culoare invalida!' + CHAR(13) + CHAR(10)
 
 	IF (dbo.uf_ValidareMaterial(@material) = 0)
-		SET @err = @err + 'Material invalid!' + CHAR(13) + CHAR(10)
+		SET @errors = @errors + 'Material invalid!' + CHAR(13) + CHAR(10)
 
 	IF (dbo.uf_ValidareVechime(@vechime) = 0)
-		SET @err = @err + 'Vechime invalida!' + CHAR(13) + CHAR(10)
+		SET @errors = @errors + 'Vechime invalida!' + CHAR(13) + CHAR(10)
 
 	IF (dbo.uf_ValidareVitrinaID(@vitrinaID) = 0)
-		SET @err = @err + 'VitrinaID invalid!' + CHAR(13) + CHAR(10)
+		SET @errors = @errors + 'VitrinaID invalid!' + CHAR(13) + CHAR(10)
 	
-	IF (LEN(@err) <> 0)
-		RAISERROR(@err, 14, 1)
+	IF (LEN(@errors) <> 0) -- IF (LEN(@errors) != 0)
+						   -- IF (LEN(@errors) > 0)
+						   -- IF (LEN(@errors) >= 1)
+		RAISERROR(@errors, 14, 1)
 END
 
 GO
@@ -102,10 +104,10 @@ BEGIN
 	
 	DECLARE @vasID INT
 	SELECT @vasID = MAX(VasID)
-	FROM [MuzeuDB].[dbo].[Vase]
+	FROM Vase
 	SET @vasID = @vasID + 1
 	
-	INSERT INTO [MuzeuDB].[dbo].[Vase] (VasID, Culoare, Material, Vechime, VitrinaID) VALUES
+	INSERT INTO Vase (VasID, Culoare, Material, Vechime, VitrinaID) VALUES
 	(@vasID, @culoare, @material, @vechime, @vitrinaID)
 END
 
@@ -115,12 +117,12 @@ CREATE OR ALTER FUNCTION uf_ValidareNume (@nume VARCHAR(50))
 RETURNS BIT
 AS
 BEGIN
-	DECLARE @retval BIT = 0
+	DECLARE @valid BIT = 0
     
     IF (@nume IS NOT NULL AND LEN(@nume) <> 0 AND @nume LIKE '%[a-zA-Z]%' AND @nume NOT LIKE '%[^a-zA-Z]%')
-        SET @retval = 1
+        SET @valid = 1
 
-	RETURN @retval
+	RETURN @valid
 END
 
 GO
@@ -129,12 +131,12 @@ CREATE OR ALTER FUNCTION uf_ValidarePrenume (@prenume VARCHAR(50))
 RETURNS BIT
 AS
 BEGIN
-    DECLARE @retval BIT = 0
+    DECLARE @valid BIT = 0
     
     IF (@prenume IS NOT NULL AND LEN(@prenume) <> 0 AND @prenume LIKE '%[a-zA-Z]%' AND @prenume NOT LIKE '%[^a-zA-Z]%')
-        SET @retval = 1
+        SET @valid = 1
 
-	RETURN @retval
+	RETURN @valid
 END
 
 GO
@@ -143,15 +145,15 @@ CREATE OR ALTER FUNCTION uf_ValidareVarsta (@varsta INT)
 RETURNS BIT
 AS
 BEGIN
-	DECLARE @retval BIT = 1
+	DECLARE @valid BIT = 1
 
     IF (@varsta < 0)
-        SET @retval = 0
+        SET @valid = 0
 
 	IF (@varsta NOT BETWEEN 3 AND 120)
-		SET @retval = 0
+		SET @valid = 0
 
-	RETURN @retval
+	RETURN @valid
 END
 
 GO
@@ -160,19 +162,21 @@ CREATE OR ALTER PROCEDURE sp_ValidareVizitator
 @nume VARCHAR(50), @prenume VARCHAR(50), @varsta INT
 AS
 BEGIN
-	DECLARE @err VARCHAR(MAX) = ''
+	DECLARE @errors VARCHAR(MAX) = ''
 
 	IF (dbo.uf_ValidareNume(@nume) = 0)
-		SET @err = 'Nume invalid!' + CHAR(13) + CHAR(10)
+		SET @errors = 'Nume invalid!' + CHAR(13) + CHAR(10)
 
 	IF (dbo.uf_ValidarePrenume(@prenume) = 0)
-		SET @err = @err + 'Prenume invalid!' + CHAR(13) + CHAR(10)
+		SET @errors = @errors + 'Prenume invalid!' + CHAR(13) + CHAR(10)
 
 	IF (dbo.uf_ValidareVarsta(@varsta) = 0)
-		SET @err = @err + 'Varsta invalida!' + CHAR(13) + CHAR(10)
+		SET @errors = @errors + 'Varsta invalida!' + CHAR(13) + CHAR(10)
 	
-	IF (LEN(@err) <> 0)
-		RAISERROR(@err, 14, 1)
+	IF (LEN(@errors) <> 0) -- IF (LEN(@errors) != 0)
+						   -- IF (LEN(@errors) > 0)
+						   -- IF (LEN(@errors) >= 1)
+		RAISERROR(@errors, 14, 1)
 END
 
 GO
@@ -184,7 +188,7 @@ AS
 BEGIN
 	EXEC sp_ValidareVizitator @nume, @prenume, @varsta
 	
-	INSERT INTO [MuzeuDB].[dbo].[Vizitatori] (Prenume, Nume, Varsta) VALUES
+	INSERT INTO Vizitatori (Prenume, Nume, Varsta) VALUES
 	(@prenume, @nume, @varsta)
 END
 
@@ -196,13 +200,13 @@ AS
 BEGIN
 	DECLARE @nrLegitimatie INT
 	SELECT @nrLegitimatie = MAX(NrLegitimatie)
-	FROM [MuzeuDB].[dbo].[Vizitatori]
+	FROM Vizitatori
 
 	DECLARE @vasID INT
 	SELECT @vasID = MAX(VasID)
-	FROM [MuzeuDB].[dbo].[Vase]
+	FROM Vase
 
-	INSERT INTO [MuzeuDB].[dbo].[VizitatoriVase] (NrLegitimatie, VasID) VALUES
+	INSERT INTO VizitatoriVase (NrLegitimatie, VasID) VALUES
 	(@nrLegitimatie, @vasID)
 END
 
@@ -236,9 +240,9 @@ GO
 CREATE OR ALTER PROCEDURE sp_StatusTabele
 AS
 BEGIN
-	SELECT * FROM [MuzeuDB].[dbo].[Vase]
-	SELECT * FROM [MuzeuDB].[dbo].[Vizitatori]
-	SELECT * FROM [MuzeuDB].[dbo].[VizitatoriVase]
+	SELECT * FROM Vase
+	SELECT * FROM Vizitatori
+	SELECT * FROM VizitatoriVase
 END
 
 GO
@@ -255,7 +259,7 @@ END
 
 GO
 
-SELECT * FROM [MuzeuDB].[dbo].[Vitrine]
+SELECT * FROM Vitrine
 
 -- APELURI CU SUCCES
 EXEC sp_InserareVaseVizitatoriWrapper 'Verde', 'Ceramica', 2375, 10, 'Jane', 'Doe', 45
